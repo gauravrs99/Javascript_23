b0a2c575eff08eb7767eeef1622a8b1c453f9642
gauravrs99 <105037034+gauravrs99@users.noreply.github.com>
2023-02-06 07:06:59 +0000

    Datatypes & ECMA Script

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Gaurav")
+
+
+let name = "gaurav"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// Different Datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
